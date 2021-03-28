e5708aa8c7acc0ee95e8071686e2c3b30e3942f3
Paco Lopez <francisco.lopsan@gmail.com>
2021-03-28 22:48:40 +0200

    Change 'tracking_id'

--- src/config/analytics/index.js
+++ src/config/analytics/index.js
@@ -1,6 +1,6 @@
 import ReactGA from "react-ga";
 
 export const configureAnalytics = () => {
-  const TRACKING_ID = "G-MCBD24VGWW"; // YOUR_OWN_TRACKING_ID
+  const TRACKING_ID = "G-HDH8D0B2PZ"; // YOUR_OWN_TRACKING_ID
   ReactGA.initialize(TRACKING_ID);
 };

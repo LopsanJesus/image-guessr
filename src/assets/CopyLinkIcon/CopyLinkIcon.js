720bbf9b6b94200717005c987afb899edd113cc9
Paco Lopez <francisco.lopsan@gmail.com>
2021-04-04 01:46:04 +0200

    Add CopyLinkIcon logic

--- src/assets/CopyLinkIcon/CopyLinkIcon.js
+++ src/assets/CopyLinkIcon/CopyLinkIcon.js
@@ -1,31 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./style.css";
 
 const URL = window.location.origin;
 
 const CopyLinkIcon = () => {
+  const [showConfirmation, setShowConfirmation] = useState(false);
   const handleClick = () => {
     navigator.clipboard.writeText(URL);
+    setShowConfirmation(true);
+    setTimeout(() => {
+      setShowConfirmation(false);
+    }, 3000);
   };
 
   return (
-    <div className="copylink-icon-wrapper" onClick={handleClick}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        className="copylink-ico"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
-        />
-      </svg>
-    </div>
+    <>
+      <div className="copylink-icon-wrapper" onClick={handleClick}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          className="copylink-ico"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
+          />
+        </svg>
+      </div>
+      {showConfirmation && (
+        <span className="text-gray-500 text-sm absolute -mt-7">
+          Link copied to clipboard
+        </span>
+      )}
+    </>
   );
 };
 

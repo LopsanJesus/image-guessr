026ca8a4f4f459fabca88c2c3850ce83f3b02637
Paco Lopez <francisco.lopsan@gmail.com>
2021-04-04 11:23:36 +0200

    Sticky bar and level header

--- src/components/Level/Level.js
+++ src/components/Level/Level.js
@@ -66,66 +66,67 @@ const Level = ({ t }) => {
       <div className="main">
         <div className="px-4 sm:px-8 lg:px-16 xl:px-20 mx-auto">
           <div className="hero">
-            <div className="hero-headline flex flex-col items-center justify-center pt-12 pb-8 text-center">
-              <h1 className="font-bold text-5xl text-gray-900 m-5">
+            <div className="hero-headline flex flex-col items-center justify-center pt-2 pb-2 text-center">
+              <h1 className="font-bold text-3xl text-gray-900 m-2">
                 {t("Guess each city")}
               </h1>
-              <h1 className="font-bold text-3xl text-gray-900">
+              <h1 className="sticky top-16 bg-gray-300 z-30 w-full font-bold text-2xl text-gray-900 py-2">
                 {`${t("Level")} ${level} | ${t("Score")}: `}
                 <span id="score">{score}</span>
               </h1>
               <p
                 id="goal-message"
-                className="font-base text-base text-gray-600 m-5"
+                className="font-base text-base text-gray-600 my-1"
               >
                 {t("Achieve 10 points to unlock level")} {parseInt(level) + 1}.
               </p>
+              <section
+                id="photos"
+                className="grid grid-cols-1 md:grid-cols-4 gap-4"
+              >
+                {images.length > 0 ? (
+                  images.map((image) => {
+                    let isStored = storedCities.includes(image);
+                    return (
+                      <div
+                        key={image}
+                        className="image-container relative inline-block text-center"
+                      >
+                        <img
+                          id={image}
+                          src={"/img/" + image + ".jpg"}
+                          width="500"
+                          height="600"
+                          className={`image ${
+                            isStored
+                              ? "border-2 rounded-lg border-green-500"
+                              : "hover:opacity-75"
+                          }`}
+                          alt={image}
+                          onClick={
+                            !isStored
+                              ? () => handleImageClick(image)
+                              : undefined
+                          }
+                        />
+                        {isStored && (
+                          <div
+                            id={image + "text"}
+                            className="imagetext uppercase font-extrabold rounded-xl text-green-500 bg-white p-4 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                          >
+                            {t(image.replaceAll("-", " ").toUpperCase())}
+                          </div>
+                        )}
+                      </div>
+                    );
+                  })
+                ) : (
+                  <div className="flex justify-center text-black p-8">
+                    No images were found
+                  </div>
+                )}
+              </section>
             </div>
-
-            <section
-              id="photos"
-              className="my-5 grid grid-cols-1 md:grid-cols-4 gap-4"
-            >
-              {images.length > 0 ? (
-                images.map((image) => {
-                  let isStored = storedCities.includes(image);
-                  return (
-                    <div
-                      key={image}
-                      className="image-container relative inline-block text-center"
-                    >
-                      <img
-                        id={image}
-                        src={"/img/" + image + ".jpg"}
-                        width="500"
-                        height="600"
-                        className={`image ${
-                          isStored
-                            ? "border-2 rounded-lg border-green-500"
-                            : "hover:opacity-75"
-                        }`}
-                        alt={image}
-                        onClick={
-                          !isStored ? () => handleImageClick(image) : undefined
-                        }
-                      />
-                      {isStored && (
-                        <div
-                          id={image + "text"}
-                          className="imagetext uppercase font-extrabold rounded-xl text-green-500 bg-white p-4 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-                        >
-                          {t(image.replaceAll("-", " ").toUpperCase())}
-                        </div>
-                      )}
-                    </div>
-                  );
-                })
-              ) : (
-                <div className="flex justify-center text-black p-8">
-                  No images were found
-                </div>
-              )}
-            </section>
           </div>
         </div>
       </div>

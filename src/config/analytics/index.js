e215a092126b37432d435d3ed5c743088f6df060
Jesús López Sánchez <lopsanjesus@gmail.com>
2021-04-05 00:38:26 +0200

    New Tracking ID

--- src/config/analytics/index.js
+++ src/config/analytics/index.js
@@ -1,6 +1,6 @@
 import ReactGA from "react-ga";
 
 export const configureAnalytics = () => {
-  const TRACKING_ID = "UA-193183415-1"; // YOUR_OWN_TRACKING_ID
+  const TRACKING_ID = "UA-193183415-2"; // YOUR_OWN_TRACKING_ID
   ReactGA.initialize(TRACKING_ID);
 };

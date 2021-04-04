75c8e5eaa1f832d816fad9ece626e7c99047d94b
Paco Lopez <francisco.lopsan@gmail.com>
2021-04-04 12:30:07 +0200

    Fix AlertModal

--- src/components/AlertModal/AlertModal.js
+++ src/components/AlertModal/AlertModal.js
@@ -12,7 +12,7 @@ const AlertModal = ({ level, setShowModal, t }) => {
 
   return (
     <div id="modal-level">
-      <div className="fixed z-10 inset-0 overflow-y-auto">
+      <div className="fixed z-30 inset-0 overflow-y-auto">
         <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
           <div className="fixed inset-0 transition-opacity" aria-hidden="true">
             <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
@@ -24,12 +24,12 @@ const AlertModal = ({ level, setShowModal, t }) => {
             &#8203;
           </span>
           <div
-            className="inline-block bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-3xl"
+            className="inline-block bg-gray-50 rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-3xl"
             role="dialog"
             aria-modal="true"
             aria-labelledby="modal-headline"
           >
-            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div className="bg-gray-50 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
               <div className="sm:flex sm:items-start">
                 <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                   <h3 className="text-lg leading-6 font-medium text-gray-900">
@@ -71,7 +71,7 @@ const AlertModal = ({ level, setShowModal, t }) => {
                 {t("Let me finish")}
               </button>
             </div>
-            <div className="mt-5">
+            <div className="my-5 bg-gray-50">
               <ShareButtons />
             </div>
           </div>

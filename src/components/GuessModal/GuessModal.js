765bc70a198611be9dab21769932e95e5302dbd7
Francisco Lopez <francisco.lopsan@gmail.com>
2021-04-25 17:16:53 +0200

    Fix GuessModal styles (again)

--- src/components/GuessModal/GuessModal.js
+++ src/components/GuessModal/GuessModal.js
@@ -72,17 +72,15 @@ const GuessModal = ({
             aria-modal="true"
             aria-labelledby="modal-headline"
           >
-            <h3 className="font-bold text-center text-2xl text-gray-900 mt-4">
-              {t(imageType)}
-            </h3>
             <div className="px-4 pt-5 text-center sm:p-6 sm:pb-4">
               <Image
+                showTypeInHeader={true}
                 imageName={imageName}
                 imageType={imageType}
                 isStored={isStored}
               />
             </div>
-            <div className="px-4 py-4 sm:px-6 sm:flex sm:flex-row sm:justify-center space-x-3">
+            <div className="px-4 py-4 sm:px-6 sm:flex sm:flex-row sm:justify-center sm:space-x-4">
               {!isStored && (
                 <>
                   <input
@@ -91,7 +89,7 @@ const GuessModal = ({
                     name="name"
                     maxLength="30"
                     size="20"
-                    className={`uppercase w-full justify-center rounded-md border shadow-sm px-4 xl:mb-3 text-base font-medium text-black focus:outline-none sm:w-auto sm:text-sm sm:leading-10 ${
+                    className={`uppercase w-full justify-center rounded-md border shadow-sm px-4 py-2 sm:py-0 xl:py-0 mb-3 text-base font-medium text-black focus:outline-none sm:w-auto sm:text-sm sm:leading-10 ${
                       error ? "bg-red-400" : ""
                     }`}
                     ref={modalInput}
@@ -107,7 +105,7 @@ const GuessModal = ({
                   <button
                     id="check-modal-button"
                     type="button"
-                    className="xl:mb-3 w-full justify-center rounded-md border border-transparent shadow-sm px-4 bg-green-600 text-base font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:w-auto sm:text-sm"
+                    className="mb-3 mt-3 w-full justify-center rounded-md border border-transparent shadow-sm px-4 py-2 sm:py-0 xl:py-0 bg-green-600 text-base font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:mt-0 sm:w-auto sm:text-sm"
                     onClick={() => {
                       checkGuess();
                     }}
@@ -119,7 +117,7 @@ const GuessModal = ({
               <button
                 id="close-modal-button"
                 type="button"
-                className="xl:mb-3 mt-3 w-full justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm"
+                className="mb-3 mt-3 w-full justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 sm:py-0 xl:py-0 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm"
                 onClick={() => {
                   setShowModal(false);
                 }}

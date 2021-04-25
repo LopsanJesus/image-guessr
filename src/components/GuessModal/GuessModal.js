daa88b23fda1804bf43700098c037d3fe551a426
Francisco Lopez <francisco.lopsan@gmail.com>
2021-04-25 15:21:04 +0200

    Fix Cancel button height

--- src/components/GuessModal/GuessModal.js
+++ src/components/GuessModal/GuessModal.js
@@ -119,7 +119,7 @@ const GuessModal = ({
               <button
                 id="close-modal-button"
                 type="button"
-                className="xl:mb-3 mt-3 w-full justify-center rounded-md border border-gray-300 shadow-sm px-4 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm"
+                className="xl:mb-3 mt-3 w-full justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm"
                 onClick={() => {
                   setShowModal(false);
                 }}

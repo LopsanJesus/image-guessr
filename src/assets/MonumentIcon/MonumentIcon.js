8532a4ac43ab7f3d5f2b7b6da22f2044b5fe5f53
Paco Lopez <francisco.lopsan@gmail.com>
2021-05-03 21:29:09 +0200

    Fix Console warnings

--- src/assets/MonumentIcon/MonumentIcon.js
+++ src/assets/MonumentIcon/MonumentIcon.js
@@ -4,7 +4,7 @@ const MonumentIcon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      class="h-7 w-7"
+      className="h-7 w-7"
       fill="none"
       viewBox="0 0 24 24"
       stroke="white"

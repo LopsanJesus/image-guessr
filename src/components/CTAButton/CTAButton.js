0c92bca534e51af7239be4b408770f42f46ab96e
Paco Lopez <francisco.lopsan@gmail.com>
2021-05-08 20:17:01 +0200

    Fix console warning

--- src/components/CTAButton/CTAButton.js
+++ src/components/CTAButton/CTAButton.js
@@ -8,7 +8,7 @@ const CTAButton = ({ text, onClick, isActive = true }) => {
       className={`bg-secondary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 w-full justify-center rounded-md border border-transparent shadow-sm px-4 py-2 text-primary font-bold text-base font-medium text-white sm:ml-3 sm:w-auto sm:text-sm ${
         !isActive ? "opacity-30" : ""
       }`}
-      onClick={isActive && onClick}
+      onClick={isActive ? onClick : undefined}
       disabled={!isActive}
     >
       {text}

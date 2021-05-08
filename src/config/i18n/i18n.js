4f2e9b04aba65e067e2f1c86702df18659f2bb1a
Paco Lopez <francisco.lopsan@gmail.com>
2021-05-08 17:20:50 +0200

    Revert changes on netlify.toml

--- src/config/i18n/i18n.js
+++ src/config/i18n/i18n.js
@@ -1,26 +1,22 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 // import { resources } from "./translations";
-import languageDetector from "i18next-browser-languagedetector";
 import en from "./translations/en.json";
 import es from "./translations/es.json";
 
-i18n
-  .use(initReactI18next)
-  .use(languageDetector)
-  .init({
-    resources: {
-      en: en,
-      es: es,
-    },
+i18n.use(initReactI18next).init({
+  resources: {
+    en: en,
+    es: es,
+  },
 
-    supportedLngs: ["en", "es"],
-    fallbackLng: "es",
-    keySeparator: false, // we do not use keys in form messages.welcome
+  supportedLngs: ["en", "es"],
+  fallbackLng: "es",
+  keySeparator: false, // we do not use keys in form messages.welcome
 
-    interpolation: {
-      escapeValue: false, // react already safes from xss
-    },
-  });
+  interpolation: {
+    escapeValue: false, // react already safes from xss
+  },
+});
 
 export default i18n;
